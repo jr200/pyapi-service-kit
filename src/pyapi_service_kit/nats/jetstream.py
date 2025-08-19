4122a20e3acb28a5f2ea8661ed1d49abf0b67dcf
jr200 <jayshan+git@gmail.com>
2025-08-19 11:02:57 +0100

    added common NatsConfig class and reworked jetstream creation via config

--- src/pyapi_service_kit/nats/jetstream.py
+++ src/pyapi_service_kit/nats/jetstream.py
@@ -1,7 +1,9 @@
 import logging
-from typing import Any, Mapping
+from typing import List
 
 from nats.js.client import JetStreamContext
+
+from .config import StreamConfig
 
 
 LOGGER = logging.getLogger(__name__)
@@ -15,14 +17,19 @@ async def try_delete_stream(js: JetStreamContext, stream_name: str):
         return False
 
 
-async def create_jetstream_streams(js: JetStreamContext, config: Mapping[str, Any]):
-    for stream_name, stream_config in config.items():
-        try:
-            if stream_config.get("recreate_if_exists", False):
-                await try_delete_stream(js, stream_name)
+async def create_jetstream_streams(js: JetStreamContext, config: List[StreamConfig]):
+    for stream_config in config:
+        await _create_jetstream_stream(js, stream_config)
 
-            await js.add_stream(name=stream_name, **stream_config["options"])
-            LOGGER.info(f"JetStream stream {stream_name} created")
-        except Exception as e:
-            LOGGER.error(f"Error creating JetStream stream {stream_name}: {e}")
-            raise
+
+async def _create_jetstream_stream(js: JetStreamContext, config: StreamConfig):
+
+    try:
+        if config.recreate_if_exists:
+            await try_delete_stream(js, config.name)
+
+        await js.add_stream(name=config.name, **config.options)
+        LOGGER.info(f"JetStream stream {config.name} created")
+    except Exception as e:
+        LOGGER.error(f"Error creating JetStream stream {config.name}: {e}")
+        raise
